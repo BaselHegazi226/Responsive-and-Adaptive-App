e4650bc39072db9d2a503a6ac1a763f151c6faad
baselhegazi <baselmohammed226@gmail.com>
2025-02-04 06:24:27 +0200

    add media_query_apply widget

--- lib/widgets/media_query_apply.dart
+++ lib/widgets/media_query_apply.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class MediaQueryApply extends StatelessWidget {
+  const MediaQueryApply({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    return Column(
+      spacing: 16,
+      children: [
+        Container(
+          height: size.height * .2,
+          color: Colors.red,
+        ),
+        Container(
+          height: size.height * .2,
+          color: Colors.brown,
+        ),
+        Container(
+          height: size.height * .2,
+          color: Colors.green,
+        ),
+      ],
+    );
+  }
+}
