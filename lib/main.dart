159c68e877fac2f7513637d4dd82bb03541ab00f
baselhegazi <baselmohammed226@gmail.com>
2025-02-05 21:46:24 +0200

    import links

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_app_test_1/widgets/layout_widgets/layout_builder_app.dart';
+import 'package:responsive_app_test_1/widgets/expanded_app/expanded_app.dart';
 
 void main() {
-  runApp(const LayoutBuilderApp());
+  runApp(const ExpandedApp());
 }
