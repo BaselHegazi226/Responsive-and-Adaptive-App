c2f8636f41fc2ed5d8937baeced0bc13eadc6622
baselhegazi <baselmohammed226@gmail.com>
2025-02-05 07:07:18 +0200

    transfer the media query app to another separate file

--- lib/main.dart
+++ lib/main.dart
@@ -2,32 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:responsive_app_test_1/widgets/media_query_apply.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Adaptive & Responsive',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Title(
-            color: Colors.red,
-            child: Text(
-              'Media Query',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 24,
-              ),
-            ),
-          ),
-        ),
-        body: MediaQueryApply(),
-      ),
-    );
-  }
+  runApp(MediaQueryApp());
 }
