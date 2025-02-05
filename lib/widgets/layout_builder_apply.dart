5290c66d3a3a02a66aed1a4ea9713ba58c9ab17a
baselhegazi <baselmohammed226@gmail.com>
2025-02-05 07:17:36 +0200

    make layout_builder_apply , layout_builder_app , list tile item widgets

---
+++ lib/widgets/layout_builder_apply.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class LayoutBuilderApply extends StatelessWidget {
+  static String id = 'Layout Builder Apply';
+  const LayoutBuilderApply({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Title(
+              color: Colors.red,
+              child: Text(
+                'Layout Builder',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ),
+          body: ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTileItem(
+                index: index,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ListTileItem extends StatelessWidget {
+  const ListTileItem({
+    super.key,
+    required this.index,
+  });
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        '${index + 1}',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class LayoutBuilderApp extends StatelessWidget {
+  const LayoutBuilderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: LayoutBuilderApply.id,
+      home: LayoutBuilderApply(),
+    );
+  }
+}
