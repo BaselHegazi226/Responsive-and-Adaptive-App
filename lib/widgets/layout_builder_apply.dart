635472698a7daadab68b9722fb49f75587898154
baselhegazi <baselmohammed226@gmail.com>
2025-02-05 07:41:24 +0200

    update

--- lib/widgets/layout_builder_apply.dart
+++ lib/widgets/layout_builder_apply.dart
@@ -1,64 +1,5 @@
 import 'package:flutter/material.dart';
 
-class LayoutBuilderApply extends StatelessWidget {
-  static String id = 'Layout Builder Apply';
-  const LayoutBuilderApply({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Title(
-              color: Colors.red,
-              child: Text(
-                'Layout Builder',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-          ),
-          body: ListView.builder(
-            itemBuilder: (context, index) {
-              return ListTileItem(
-                index: index,
-              );
-            },
-          ),
-        );
-      },
-    );
-  }
-}
-
-class ListTileItem extends StatelessWidget {
-  const ListTileItem({
-    super.key,
-    required this.index,
-  });
-  final int index;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber.withAlpha(128),
-      height: 24,
-      child: ListTile(
-        title: Text(
-          '${index + 1}',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
 class LayoutBuilderApp extends StatelessWidget {
   const LayoutBuilderApp({super.key});
 
@@ -66,7 +7,66 @@ class LayoutBuilderApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: LayoutBuilderApply.id,
-      home: LayoutBuilderApply(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LayoutBuilderApply(),
+    );
+  }
+}
+
+class LayoutBuilderApply extends StatelessWidget {
+  static String id = 'Layout Builder Apply';
+  const LayoutBuilderApply({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Layout Builder',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTileItem(index: index);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ListTileItem extends StatelessWidget {
+  final int index;
+  const ListTileItem({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        bottom: 16,
+      ),
+      color: Colors.amber,
+      height: 48,
+      child: ListTile(
+        title: Text(
+          textAlign: TextAlign.center,
+          '${index + 1}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
